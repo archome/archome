9f42f11ce9085c9966d2dc21f007ae10c2052e96
Mark A. Matienzo <mark@matienzo.org>
2012-03-05 19:58:58 -0500

    Site updated at 2012-03-06 00:58:58 UTC

--- javascripts/pinboard.js
+++ javascripts/pinboard.js
@@ -52,8 +52,5 @@ function Pinboard_Linkroll() {
   }
 }
 Pinboard_Linkroll.prototype = new Pinboard_Linkroll();
-if (pinboard_user is undefined) {
-    pinboardNS_fetch_script("http://feeds.pinboard.in/json/v1/t:"+pinboard_tag+"/?cb=pinboardNS_show_bmarks\&count="+pinboard_count);
-} else {
-    pinboardNS_fetch_script("http://feeds.pinboard.in/json/v1/u:"+pinboard_user+"/?cb=pinboardNS_show_bmarks\&count="+pinboard_count);
-}
+pinboardNS_fetch_script("http://feeds.pinboard.in/json/v1/u:"+pinboard_user+"/?cb=pinboardNS_show_bmarks\&count="+pinboard_count);
+
